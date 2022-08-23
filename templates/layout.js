5cdcef13c08f56366a8006be18db573db709f71a
oliverjam <oliverjamesphillips@gmail.com>
2022-08-23 15:03:53 -0700

    Add favicons

--- templates/layout.js
+++ templates/layout.js
@@ -6,6 +6,7 @@ export function Layout({ content, css = [] }) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Delirium & Revelry</title>
+        <link rel="icon" type="image/png" href="favicon.png">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Limelight&display=block" rel="stylesheet"> 
