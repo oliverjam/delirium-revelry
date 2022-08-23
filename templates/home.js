2c5a645c4649606d2d94c8e9d7f8d77fc46af00a
oliverjam <oliverjamesphillips@gmail.com>
2022-08-23 15:01:00 -0700

    Only publish functions/ and public/

--- templates/home.js
+++ templates/home.js
@@ -2,12 +2,12 @@ export function Home() {
   return /*html*/ `
     <main class="home-layout">
       <picture class="home-bg">
-        <source type="image/webp" srcset="/assets/img/home-bg.webp" alt="">
-        <img src="/assets/img/home-bg.jpeg" alt="">
+        <source type="image/webp" srcset="/img/home-bg.webp" alt="">
+        <img src="/img/home-bg.jpeg" alt="">
       </picture>
       <header>
         <h1>
-          <img src="/assets/img/home-logo.png" alt="Delirium & Revelry. Cocktails, world beers, private events">
+          <img src="/img/home-logo.png" alt="Delirium & Revelry. Cocktails, world beers, private events">
         </h1>
         <hr>
         <p class="subheading font-title">Coming soon August 2022</p>
