3f6e2562d53cba4e6315c238b4e0f4d43136a878
oliverjam <oliverjamesphillips@gmail.com>
2022-08-26 10:46:06 -0700

    Stop images messing with page load

--- functions/templates/home.js
+++ functions/templates/home.js
@@ -3,11 +3,16 @@ export function Home() {
     <main class="home-layout">
       <picture class="home-bg">
         <source type="image/webp" srcset="/assets/img/home-bg.webp" alt="">
-        <img src="/assets/img/home-bg.jpeg" alt="">
+        <img src="/assets/img/home-bg.jpeg" alt="" loading="lazy">
       </picture>
       <header>
         <h1>
-          <img src="/assets/img/home-logo.png" alt="Delirium & Revelry. Cocktails, world beers, private events">
+          <img
+            src="/assets/img/home-logo.png"
+            alt="Delirium & Revelry. Cocktails, world beers, private events"
+            width="1445"
+            height="468"
+          >
         </h1>
         <hr>
         <p class="subheading font-title">Coming soon August 2022</p>
